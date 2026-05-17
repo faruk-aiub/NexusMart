ce6a2ce2f3b16c5c6974173b9861acf438b454ba
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-17 08:37:27 +0600

    Add update category DTO

---
+++ backend/src/categories/dto/update-category.dto.ts
@@ -0,0 +1,19 @@
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+
+export class UpdateCategoryDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  status?: boolean;
+}

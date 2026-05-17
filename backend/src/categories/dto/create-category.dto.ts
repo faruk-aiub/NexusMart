c7bafdd4e8a226ea2481e29ac641f8568ad4d8c6
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-17 08:35:45 +0600

    Add create category DTO

---
+++ backend/src/categories/dto/create-category.dto.ts
@@ -0,0 +1,19 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  status?: boolean;
+}

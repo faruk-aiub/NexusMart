9603a501138c98aa80b401430e58d2cd3a883378
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-18 11:48:51 +0600

    Modify carts module

--- backend/src/carts/carts.module.ts
+++ backend/src/carts/carts.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CartsController } from './carts.controller';
 import { CartsService } from './carts.service';
 
+import { Cart } from './entities/cart.entity';
+import { CartItem } from './entities/cart-item.entity';
+import { Product } from '../products/entities/product.entity';
+import { User } from '../users/entities/user.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Cart, CartItem, Product, User])],
   controllers: [CartsController],
-  providers: [CartsService]
+  providers: [CartsService],
+  exports: [CartsService],
 })
 export class CartsModule {}

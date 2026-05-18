33da4a2387ec007ba6892c8b0de3a7a3fee7fa16
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-18 11:50:48 +0600

    Modify orders module

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { Cart } from '../carts/entities/cart.entity';
+import { CartItem } from '../carts/entities/cart-item.entity';
+import { Product } from '../products/entities/product.entity';
+import { User } from '../users/entities/user.entity';
+import { MailModule } from '../mail/mail.module';
+
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderItem, Cart, CartItem, Product, User]),
+    MailModule,
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
 })
 export class OrdersModule {}

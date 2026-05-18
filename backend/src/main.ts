d41a906a5ce54a725064b0b5cebd3bee09afe804
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-18 19:30:31 +0600

    Modify main file for Swagger and static uploads

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('NexusMart API')

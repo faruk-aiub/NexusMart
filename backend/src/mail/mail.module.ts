6ee3c6c7c08e0ff972ecbbf5a01bfc33fc6780ec
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-18 11:49:46 +0600

    Modify mail module configuration

--- backend/src/mail/mail.module.ts
+++ backend/src/mail/mail.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+
 import { MailService } from './mail.service';
 
 @Module({
-  providers: [MailService]
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT),
+        secure: false,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+      defaults: {
+        from: process.env.MAIL_FROM || 'nexusmart@example.com',
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}

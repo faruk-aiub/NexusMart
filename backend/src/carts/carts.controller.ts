35d18eaf1308887885ed5f04b54d41d4d7db3b20
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-18 11:48:18 +0600

    Modify carts controller with cart item routes

--- backend/src/carts/carts.controller.ts
+++ backend/src/carts/carts.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+
+import { CartsService } from './carts.service';
+import { AddCartItemDto } from './dto/add-cart-item.dto';
+import { UpdateCartItemDto } from './dto/update-cart-item.dto';
+
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+
+import { UserRole } from '../common/enums/user-role.enum';
 
 @Controller('carts')
-export class CartsController {}
+export class CartsController {
+  constructor(private cartsService: CartsService) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  @Get()
+  getMyCart(@CurrentUser() user: any) {
+    return this.cartsService.getMyCart(user.userId);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  @Post('items')
+  addItemToCart(
+    @CurrentUser() user: any,
+    @Body() addCartItemDto: AddCartItemDto,
+  ) {
+    return this.cartsService.addItemToCart(user.userId, addCartItemDto);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  @Patch('items/:id')
+  updateCartItem(
+    @CurrentUser() user: any,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCartItemDto: UpdateCartItemDto,
+  ) {
+    return this.cartsService.updateCartItem(user.userId, id, updateCartItemDto);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  @Delete('items/:id')
+  removeCartItem(
+    @CurrentUser() user: any,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.cartsService.removeCartItem(user.userId, id);
+  }
+}

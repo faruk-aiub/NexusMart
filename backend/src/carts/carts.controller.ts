0800e7b47f71907d8ac85a2d18544be52c477699
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-18 13:14:58 +0600

    Modify carts controller with Swagger documentation

--- backend/src/carts/carts.controller.ts
+++ backend/src/carts/carts.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { CartsService } from './carts.service';
 import { AddCartItemDto } from './dto/add-cart-item.dto';
@@ -21,6 +22,8 @@ import { CurrentUser } from '../auth/decorators/current-user.decorator';
 
 import { UserRole } from '../common/enums/user-role.enum';
 
+@ApiTags('Carts')
+@ApiBearerAuth('access-token')
 @Controller('carts')
 export class CartsController {
   constructor(private cartsService: CartsService) {}

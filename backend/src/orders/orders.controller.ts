3ae53faed0f1bfeda7d171220cdce5c76701eddb
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-18 11:50:26 +0600

    Modify orders controller

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
+
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+
+import { UserRole } from '../common/enums/user-role.enum';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private ordersService: OrdersService) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  @Post()
+  placeOrder(@CurrentUser() user: any, @Body() createOrderDto: CreateOrderDto) {
+    return this.ordersService.placeOrder(user.userId, createOrderDto);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  @Get('my-orders')
+  getMyOrders(@CurrentUser() user: any) {
+    return this.ordersService.getMyOrders(user.userId);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Get()
+  getAllOrders() {
+    return this.ordersService.getAllOrders();
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Get(':id')
+  getOrderById(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersService.getOrderById(id);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Patch(':id/status')
+  updateOrderStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateOrderStatusDto: UpdateOrderStatusDto,
+  ) {
+    return this.ordersService.updateOrderStatus(id, updateOrderStatusDto);
+  }
+}

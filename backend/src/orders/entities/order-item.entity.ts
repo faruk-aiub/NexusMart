3b2402dd01e62a3e766e9c63b5f71544d108fe0e
Md. Omar Faruk <omarfaruk19026@gmail.com>
2026-05-18 11:54:03 +0600

    Add order item entity

---
+++ backend/src/orders/entities/order-item.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { Order } from './order.entity';
+import { Product } from '../../products/entities/product.entity';
+
+@Entity('order_items')
+export class OrderItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  quantity: number;
+
+  @Column({ type: 'float' })
+  price: number;
+
+  @ManyToOne(() => Order, (order) => order.items)
+  order: Order;
+
+  @ManyToOne(() => Product, (product) => product.orderItems)
+  product: Product;
+}
